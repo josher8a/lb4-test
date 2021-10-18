a70f6c465b823c935f73d06762f2d2a031e019cc
Joshua Hernandez <josher8a@gmail.com>
2021-10-18 09:14:53 -0400

    Adding TodoList

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,4 +1,5 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {TodoList} from './todo-list.model';
 
 @model()
 export class Todo extends Entity {
@@ -30,6 +31,8 @@ export class Todo extends Entity {
   })
   update_at?: string;
 
+  @belongsTo(() => TodoList)
+  todoListId: number;
 
   constructor(data?: Partial<Todo>) {
     super(data);
