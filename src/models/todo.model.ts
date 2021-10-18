a0ea5f250f866b3703b044732e59985099b6f9c9
Joshua Hernandez <josher8a@gmail.com>
2021-10-18 03:18:23 -0400

    Setup

---
+++ src/models/todo.model.ts
@@ -0,0 +1,43 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isDone?: boolean;
+
+  @property({
+    type: 'date',
+  })
+  update_at?: string;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
